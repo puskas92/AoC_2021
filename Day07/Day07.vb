4805f9e654084993ed33f704e52af3cc9cb3a713
Puskas David <puskas.david92@gmail.com>
2021-12-07 06:31:36 +0100

    Day07

--- Day07/Day07.vb
+++ Day07/Day07.vb
@@ -4,29 +4,54 @@
         Dim input = Day07_ReadInput("Day07\Day07_input.txt")
 
         'part1
-        Debug.Assert(Day07_Part1(testinput) = 0)
+        Debug.Assert(Day07_Part1(testinput) = 37)
         Console.WriteLine("Day07 Part1: " & Day07_Part1(input))
 
         'part2
-        Debug.Assert(Day07_Part2(testinput) = 0)
+        Debug.Assert(Day07_Part2(testinput) = 168)
         Console.WriteLine("Day07 Part2: " & Day07_Part2(input))
 
     End Sub
-    Function Day07_ReadInput(inputpath As String) As Object
+    Function Day07_ReadInput(inputpath As String) As List(Of Integer)
         Dim sr As New IO.StreamReader(inputpath)
-        While (Not sr.EndOfStream)
-
-        End While
+        Dim line = sr.ReadLine
+        Dim output = line.Split(",").ToList.ConvertAll(Function(f) Convert.ToInt32(f))
         sr.Close()
-        Return Nothing
+        Return output
     End Function
 
 
-    Function Day07_Part1(input) As Integer
-        Return 0
+    Function Day07_Part1(input As List(Of Integer)) As Integer
+        Dim min As Integer = Int32.MaxValue
+        Dim minpos As Integer = -1
+        Dim i As Integer
+        For i = input.Min To input.Max
+            Dim fuel = input.Sum(Function(f) Math.Abs(f - i))
+            If fuel < min Then
+                min = fuel
+                minpos = i
+            End If
+        Next
+
+        Return min
+
     End Function
 
-    Function Day07_Part2(input) As Integer
-        Return 0
+    Function Day07_Part2(input As List(Of Integer)) As Integer
+        Dim min As Integer = Int32.MaxValue
+        Dim minpos As Integer = -1
+        Dim i As Integer
+        For i = input.Min To input.Max
+            Dim fuel = input.Sum(Function(f)
+                                     Dim steps = Math.Abs(f - i)
+                                     Return (1 + steps) * steps / 2
+                                 End Function)
+            If fuel < min Then
+                min = fuel
+                minpos = i
+            End If
+        Next
+
+        Return min
     End Function
 End Module
---
+++ Day08/Day08.vb
@@ -0,0 +1,32 @@
+﻿Module Day08
+    Sub Day08_main()
+        Dim testinput = Day08_ReadInput("Day08\Day08_test01.txt")
+        Dim input = Day08_ReadInput("Day08\Day08_input.txt")
+
+        'part1
+        Debug.Assert(Day08_Part1(testinput) = 0)
+        Console.WriteLine("Day08 Part1: " & Day08_Part1(input))
+
+        'part2
+        Debug.Assert(Day08_Part2(testinput) = 0)
+        Console.WriteLine("Day08 Part2: " & Day08_Part2(input))
+
+    End Sub
+    Function Day08_ReadInput(inputpath As String) As Object
+        Dim sr As New IO.StreamReader(inputpath)
+        While (Not sr.EndOfStream)
+
+        End While
+        sr.Close()
+        Return Nothing
+    End Function
+
+
+    Function Day08_Part1(input) As Integer
+        Return 0
+    End Function
+
+    Function Day08_Part2(input) As Integer
+        Return 0
+    End Function
+End Module
