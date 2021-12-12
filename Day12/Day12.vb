b01fcad616474ba1443100f3864fb94ebaedf9eb
Puskas David <puskas.david92@gmail.com>
2021-12-12 07:07:11 +0100

    Day12

--- Day12/Day12.vb
+++ Day12/Day12.vb
@@ -1,32 +1,98 @@
 ﻿Module Day12
     Sub Day12_main()
-        Dim testinput = Day12_ReadInput("Day12\Day12_test01.txt")
+        Dim testinput01 = Day12_ReadInput("Day12\Day12_test01.txt")
+        Dim testinput02 = Day12_ReadInput("Day12\Day12_test02.txt")
+        Dim testinput03 = Day12_ReadInput("Day12\Day12_test03.txt")
         Dim input = Day12_ReadInput("Day12\Day12_input.txt")
 
         'part1
-        Debug.Assert(Day12_Part1(testinput) = 0)
+        Debug.Assert(Day12_Part1(testinput01) = 10)
+        Debug.Assert(Day12_Part1(testinput02) = 19)
+        Debug.Assert(Day12_Part1(testinput03) = 226)
         Console.WriteLine("Day12 Part1: " & Day12_Part1(input))
 
         'part2
-        Debug.Assert(Day12_Part2(testinput) = 0)
+        Debug.Assert(Day12_Part2(testinput01) = 36)
+        Debug.Assert(Day12_Part2(testinput02) = 103)
+        Debug.Assert(Day12_Part2(testinput03) = 3509)
         Console.WriteLine("Day12 Part2: " & Day12_Part2(input))
 
     End Sub
-    Function Day12_ReadInput(inputpath As String) As Object
+    Function Day12_ReadInput(inputpath As String) As Dictionary(Of String, List(Of String))
+        Dim result As New Dictionary(Of String, List(Of String))
         Dim sr As New IO.StreamReader(inputpath)
         While (Not sr.EndOfStream)
-
+            Dim pairs = sr.ReadLine.Split("-")
+            If result.ContainsKey(pairs(0).Trim) = False Then result.Add(pairs(0).Trim, New List(Of String))
+            If result.ContainsKey(pairs(1).Trim) = False Then result.Add(pairs(1).Trim, New List(Of String))
+            result(pairs(0)).Add(pairs(1))
+            result(pairs(1)).Add(pairs(0))
         End While
+        For Each row In result
+            row.Value.Sort()
+        Next
         sr.Close()
-        Return Nothing
+        Return result
     End Function
 
 
-    Function Day12_Part1(input) As Integer
-        Return 0
+    Function Day12_Part1(input As Dictionary(Of String, List(Of String))) As Integer
+        Dim Seen As New List(Of String)
+        Dim Routes As List(Of List(Of String)) = Day12_IterateThroughCaves1(input, "start", Seen)
+        Return Routes.Count
     End Function
 
-    Function Day12_Part2(input) As Integer
-        Return 0
+    Private Function Day12_IterateThroughCaves1(input As Dictionary(Of String, List(Of String)), CurrentPosition As String, seen As List(Of String)) As List(Of List(Of String))
+        Dim newSeen As New List(Of String)(seen)
+        newSeen.Add(CurrentPosition)
+
+        Dim result As New List(Of List(Of String))
+
+        If CurrentPosition = "end" Then
+            result.Add(newSeen)
+        Else
+            For Each dest In input(CurrentPosition)
+                If dest.ToLower = dest AndAlso seen.Contains(dest) Then Continue For
+
+                result.AddRange(Day12_IterateThroughCaves1(input, dest, newSeen))
+            Next
+        End If
+        Return result
+    End Function
+
+    Function Day12_Part2(input As Dictionary(Of String, List(Of String))) As Integer
+        Dim Seen As New List(Of String)
+        Dim Routes As List(Of List(Of String)) = Day12_IterateThroughCaves2(input, "start", Seen, "")
+        'For Each row In Routes
+        '    Console.WriteLine(String.Join(",", row))
+        'Next
+        Return Routes.Count
+    End Function
+
+    Private Function Day12_IterateThroughCaves2(input As Dictionary(Of String, List(Of String)), CurrentPosition As String, seen As List(Of String), JokerSmallCave As String) As List(Of List(Of String))
+        Dim newSeen As New List(Of String)(seen)
+        newSeen.Add(CurrentPosition)
+
+        Dim result As New List(Of List(Of String))
+        Dim newJoker As String
+
+
+        If CurrentPosition = "end" Then
+            result.Add(newSeen)
+        Else
+            For Each dest In input(CurrentPosition)
+                newJoker = JokerSmallCave
+                If dest.ToLower = dest AndAlso seen.Contains(dest) Then
+                    If dest <> "start" AndAlso dest <> "end" AndAlso JokerSmallCave = "" Then
+                        newJoker = dest
+                    Else
+                        Continue For
+                    End If
+                End If
+
+                result.AddRange(Day12_IterateThroughCaves2(input, dest, newSeen, newJoker))
+            Next
+        End If
+        Return result
     End Function
 End Module
---
+++ Day13/Day13.vb
@@ -0,0 +1,32 @@
+﻿Module Day13
+    Sub Day13_main()
+        Dim testinput = Day13_ReadInput("Day13\Day13_test01.txt")
+        Dim input = Day13_ReadInput("Day13\Day13_input.txt")
+
+        'part1
+        Debug.Assert(Day13_Part1(testinput) = 0)
+        Console.WriteLine("Day13 Part1: " & Day13_Part1(input))
+
+        'part2
+        Debug.Assert(Day13_Part2(testinput) = 0)
+        Console.WriteLine("Day13 Part2: " & Day13_Part2(input))
+
+    End Sub
+    Function Day13_ReadInput(inputpath As String) As Object
+        Dim sr As New IO.StreamReader(inputpath)
+        While (Not sr.EndOfStream)
+
+        End While
+        sr.Close()
+        Return Nothing
+    End Function
+
+
+    Function Day13_Part1(input) As Integer
+        Return 0
+    End Function
+
+    Function Day13_Part2(input) As Integer
+        Return 0
+    End Function
+End Module
